ef1cfce86f2be3a4c8569c83a75cddcc2b1eb49e
Daniel <daniel.m.smith252.civ@army.mil>
2022-03-07 15:53:23 +0000

    Distro A, OPSEC #4584. You may have additional rights; please see https://rosmilitary.org/faq/?category=ros-2-license
    Testing

--- rclrs/src/node/publisher.rs
+++ rclrs/src/node/publisher.rs
@@ -82,7 +82,7 @@ where
             )
             .ok()?;
         }
-
+ 
         let handle = Arc::new(PublisherHandle {
             handle: Mutex::new(publisher_handle),
             node_handle: node.handle.clone(),
